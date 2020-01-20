1f743e0dab5564866929dc84bb9a8bfc1420a6cc
thiagoabreudev <thiagoabreu.dev@gmail.com>
2020-01-20 23:00:33 +0000

    Deploying to master from  - 1067559de2dd13531f097617874d2df8a13b20f9 🚀

---
+++ precache-manifest.6a693bdefd0f5426d72980526eb05d44.js
@@ -0,0 +1,88 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "url": "/02095397cf5e39c66049.js"
+  },
+  {
+    "url": "/02095397cf5e39c66049.js.LICENSE"
+  },
+  {
+    "url": "/06a00dcdcf4dfd210657.js"
+  },
+  {
+    "url": "/3a9dd65f4381e3f2f15e.js"
+  },
+  {
+    "url": "/3a9dd65f4381e3f2f15e.js.LICENSE"
+  },
+  {
+    "url": "/5ba74a501acc45811977.js"
+  },
+  {
+    "url": "/5fd3b0872b4800e5cc31.js"
+  },
+  {
+    "url": "/5fd3b0872b4800e5cc31.js.LICENSE"
+  },
+  {
+    "url": "/844b5c00f89edbbe5aaa.js"
+  },
+  {
+    "url": "/8be48107553d587c3eea.js"
+  },
+  {
+    "revision": "0f0d5a0340761f2e2a3d",
+    "url": "/admin.js"
+  },
+  {
+    "revision": "80f7af4d0a827381c25623ceda5ca90a",
+    "url": "/admin.js.LICENSE"
+  },
+  {
+    "revision": "438393160dd6dbf864d3",
+    "url": "/checkout.js"
+  },
+  {
+    "revision": "2189e9b93e0716399fab77dfabf220d2",
+    "url": "/checkout.js.LICENSE"
+  },
+  {
+    "url": "/d994a59ff3d0032de6c7.js"
+  },
+  {
+    "url": "/e96bd6549a941845ba08.js"
+  },
+  {
+    "url": "/f6eb09cbea28df2a192f.js"
+  },
+  {
+    "url": "/icon_128x128.c58f8562c132df87ecb10c0598db083c.png"
+  },
+  {
+    "url": "/icon_192x192.d3b4a59e45ac4460fb8bca55139e47f4.png"
+  },
+  {
+    "url": "/icon_384x384.0240fb21e4a86ec97b70a10a0be82bb6.png"
+  },
+  {
+    "url": "/icon_512x512.a0ba80003b3cf0f275a47885c5bf31c0.png"
+  },
+  {
+    "url": "/icon_96x96.570e1710e5824f55c3c7a5c606cbfc9c.png"
+  },
+  {
+    "revision": "74c369e638b61d85d7db310c53781c41",
+    "url": "/manifest.json"
+  },
+  {
+    "revision": "fe5a535d1544273a265a",
+    "url": "/storefront.css"
+  },
+  {
+    "revision": "fe5a535d1544273a265a",
+    "url": "/storefront.js"
+  },
+  {
+    "revision": "4afe187c776b0d05d44293da6186e677",
+    "url": "/storefront.js.LICENSE"
+  }
+]);
